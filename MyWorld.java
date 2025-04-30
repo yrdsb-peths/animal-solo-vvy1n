bb4c49ba9cd4d36197d524c1ccfc5fab0279ab30
vyin <limcolin0809@gmail.com>
2025-04-30 08:31:14 -0400

    added rhino + pumpkin

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Rhino rhino = new Rhino();
+        addObject(rhino, 300, 200);
+        
+        Pumpkin pumpkin = new Pumpkin();
+        addObject(pumpkin, 300, 0);
     }
 }
---
+++ Rhino.java
@@ -0,0 +1,14 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class Rhino extends Actor
+{
+    /**
+     * Act - do whatever the Rhino wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}
